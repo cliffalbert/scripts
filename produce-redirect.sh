efe91b0b8dcdbfc766c29ece039089fd458e8e97
Cliff Albert <cliff.albert@gmail.com>
2018-09-25 14:43:38 +0200

    produce-redirect.sh: redirect script for nginx

---
+++ produce-redirect.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [ ! "$#" -eq 2 ]; then
+	echo "$0 hostname redirect-url"
+ 	exit
+fi
+
+HOSTNAME=$1
+REDIRECT=$2
+
+if host -t A ${HOSTNAME} ; then echo OK; else echo "Domain does not resolve"; exit; fi
+
+echo "
+server {
+    listen 80;
+    listen [::]:80;
+    server_name $1;
+    access_log /var/log/nginx/$1-access.log;
+    error_log /var/log/nginx/$1-error.log;
+    return 301 $2;
+}
+" > /etc/nginx/sites-enabled/$1.conf
+
+echo "$1;$2" >> /etc/nginx/redirects.csv
