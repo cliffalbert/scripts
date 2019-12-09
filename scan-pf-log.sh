c53e55f674a5e8feff4c2f0affbae5e2b034aad7
Cliff Albert <cliff.albert@gmail.com>
2019-12-09 12:36:16 +0100

    Check for logfile existence

--- scan-pf-log.sh
+++ scan-pf-log.sh
@@ -3,8 +3,12 @@
 LOGFILE=$1
 LOGSEARCH=$2
 
-if echo "$LOGFILE" | grep gz; then 
-  for i in $(sudo zcat "${LOGFILE}" | grep -i "${LOGSEARCH}" | awk '{print $6}' | cut -d':' -f1 | grep -E '^[A-F0-9]'); do sudo zcat "${LOGFILE}" | grep "$i"; done
+if [ -e "$LOGFILE" ]; then
+  if echo "$LOGFILE" | grep gz; then 
+    for i in $(sudo zcat "${LOGFILE}" | grep -i "${LOGSEARCH}" | awk '{print $6}' | cut -d':' -f1 | grep -E '^[A-F0-9]'); do sudo zcat "${LOGFILE}" | grep "$i"; done
+  else
+    for i in $(sudo cat "${LOGFILE}" | grep -i "${LOGSEARCH}" | awk '{print $6}' | cut -d':' -f1 | grep -E '^[A-F0-9]'); do sudo cat "${LOGFILE}" | grep "$i"; done
+  fi
 else
-  for i in $(sudo cat "${LOGFILE}" | grep -i "${LOGSEARCH}" | awk '{print $6}' | cut -d':' -f1 | grep -E '^[A-F0-9]'); do sudo cat "${LOGFILE}" | grep "$i"; done
+  echo "Logfile $LOGFILE does not exist"
 fi
