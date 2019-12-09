cba5968a62b975ac4236e72ed2d5f824b7d4d1a5
Cliff Albert <cliff.albert@gmail.com>
2019-12-09 12:13:15 +0100

    ShellChecked

--- scan-pf-log.sh
+++ scan-pf-log.sh
@@ -3,9 +3,8 @@
 LOGFILE=$1
 LOGSEARCH=$2
 
-if echo $LOGFILE | grep gz; then 
-  for i in `sudo zcat ${LOGFILE} | grep -i ${LOGSEARCH} | awk '{print $6}' | cut -d':' -f1 | egrep '^[A-F0-9]'`; do sudo zcat ${LOGFILE} | grep $i; done
+if echo "$LOGFILE" | grep gz; then 
+  for i in $(sudo zcat "${LOGFILE}" | grep -i "${LOGSEARCH}" | awk '{print $6}' | cut -d':' -f1 | grep -E '^[A-F0-9]'); do sudo zcat "${LOGFILE}" | grep "$i"; done
 else
-  for i in `sudo cat ${LOGFILE} | grep -i ${LOGSEARCH} | awk '{print $6}' | cut -d':' -f1 | egrep '^[A-F0-9]'`; do sudo cat ${LOGFILE} | grep $i; done
+  for i in $(sudo cat "${LOGFILE}" | grep -i "${LOGSEARCH}" | awk '{print $6}' | cut -d':' -f1 | grep -E '^[A-F0-9]'); do sudo cat "${LOGFILE}" | grep "$i"; done
 fi
- 
